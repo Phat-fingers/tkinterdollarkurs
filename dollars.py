404756970d1da417074458c4ede34b22fd4e5c19
Phat-fingers <anders.peterssson@gmail.com>
2020-03-21 23:05:52 +0100

    N/A

--- dollars.py
+++ dollars.py
@@ -13,8 +13,19 @@ def kursScrape():
     dk = float(data.replace(',', '.'))
 
     print(dk)
-
+#TODO Bättre layout!
 root = Tk()
 root.title('Valutaomvandlare 1.0')
-root.geometry('400x150')
+root.geometry('250x100')
 
+ram = Frame(bd=0, relief=FLAT)
+ram.grid(row=0, column=0, pady=5, padx=5)
+
+sek = Label(ram, text='Ange sek:')
+sek.grid(row=0, column=0, sticky=W)
+sek.entry = Entry(ram)
+sek.entry.grid(row=0, column=1)
+
+btn1 = ttk.Button(root, text='Växla')
+btn1.grid(row=1, column=1, sticky=S)
+root.mainloop()
