70946b866926fab24e4109dc3f2e1ec632a6c28f
Phat-fingers <anders.peterssson@gmail.com>
2020-03-22 13:44:00 +0100

    Now its working

--- dollars.py
+++ dollars.py
@@ -4,28 +4,50 @@ import requests
 from bs4 import BeautifulSoup
 
 def kursScrape():
-
+    global dollarKurs
     url = requests.get('https://www.di.se/valutor/usdsek-26491/').text
     soup = BeautifulSoup(url, 'html.parser')
 
     data = soup.find('span', {'class': 'js_real-time-stock-details-price'}).text
 
-    dk = float(data.replace(',', '.'))
+    dollarKurs = float(data.replace(',', '.'))
 
-    print(dk)
+    return dollarKurs
+kursScrape()
+def count():
+    getSek = entry.get()
+    getSek = float(getSek)
+
+    try:
+        exchange = round(getSek / dollarKurs, 2)
+        info['text'] = f'Du får {exchange} dollar!'
+    except:
+        print('Funkar inte')
+
+
+
 #TODO Bättre layout!
 root = Tk()
 root.title('Valutaomvandlare 1.0')
-root.geometry('250x100')
+root.geometry('245x147')
 
-ram = Frame(bd=0, relief=FLAT)
-ram.grid(row=0, column=0, pady=5, padx=5)
 
-sek = Label(ram, text='Ange sek:')
-sek.grid(row=0, column=0, sticky=W)
-sek.entry = Entry(ram)
-sek.entry.grid(row=0, column=1)
+head = Label(root, text='Valutaomvandlare')
+head.grid(row=0, column=1)
+head1 = Label(root, text='Sek till Dollar')
+head1.grid(row=1, column=1)
 
-btn1 = ttk.Button(root, text='Växla')
-btn1.grid(row=1, column=1, sticky=S)
+ange = Label(root, text='Ange sek:')
+ange.grid(sticky=E)
+
+entry = Entry(root)
+entry.grid(row=2, column=1, padx=5)
+
+
+btn1 = ttk.Button(root, text='Växla', command=count)
+btn1.grid(row=4, column=1, pady=20, padx=5)
+
+info = Label(root)
+info.grid(row=3, column=1)
+
 root.mainloop()
